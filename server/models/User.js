46b1c35e08b338872a0998107feedbc100170937
vadagammanikanta <vadagammanikanta2006@gmail.com>
2026-06-07 17:22:06 +0530

    fix: resolve TypeError by removing next callback from async mongoose pre-save hook

--- server/models/User.js
+++ server/models/User.js
@@ -21,17 +21,12 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Compare password
